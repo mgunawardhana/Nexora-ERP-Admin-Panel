92a21143c5b8d7ee376c43c09e7c919392cd206d
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-10-02 21:41:43 +0530

    content update

--- src/app/axios/services/mega-city-services/user-management-service/UserService.tsx
+++ src/app/axios/services/mega-city-services/user-management-service/UserService.tsx
@@ -4,7 +4,6 @@ import * as url from '../url_helper';
 export const fetchAllUsersByPagination = (page: string | number, size: string | number) =>
 	post(`${url.FETCH_ALL_USERS_BY_PAGINATION}page=${page}&size=${size}`);
 
-
 export const registerUser = (data: any) => post(url.REGISTER_USER, data);
 
-export const updateUser = (data: any) => post(url.UPDATE_USER, data);
+export const updateUser = (id: any, data: any) => post(`${url.UPDATE_USER}${id}`, data);
